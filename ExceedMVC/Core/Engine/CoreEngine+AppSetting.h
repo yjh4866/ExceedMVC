58883a2f42027232643693898a0faa6fb8404ae8
yjh4866 <yjh4866@163.com>
2013-06-07 21:47:10 +0800

    为CoreEngine增加两个分类
    为CoreEngine加个AppSetting和FileManager的分类，UIEngine访问AppSetting和FileManager也
    可通过CoreEngine的这两个分类，彻底切断UI模块与其他模块的关系

---
+++ ExceedMVC/Core/Engine/CoreEngine+AppSetting.h
@@ -0,0 +1,13 @@
+//
+//  CoreEngine+AppSetting.h
+//  ExceedMVC
+//
+//  Created by yangjh on 13-6-7.
+//  Copyright (c) 2013年 yjh4866. All rights reserved.
+//
+
+#import "CoreEngine.h"
+
+@interface CoreEngine (AppSetting)
+
+@end
---
+++ ExceedMVC/Core/Engine/CoreEngine+AppSetting.m
@@ -0,0 +1,13 @@
+//
+//  CoreEngine+AppSetting.m
+//  ExceedMVC
+//
+//  Created by yangjh on 13-6-7.
+//  Copyright (c) 2013年 yjh4866. All rights reserved.
+//
+
+#import "CoreEngine+AppSetting.h"
+
+@implementation CoreEngine (AppSetting)
+
+@end
