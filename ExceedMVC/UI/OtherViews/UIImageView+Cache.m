ba8d3a4955d3bb67846845537c41d8824994761c
jianhong <jianhong.yang@chukong-inc.com>
2015-09-17 15:24:40 +0800

    修改url转换为文件名的方案，将该url按md5处理即可，这样可以解决原方案文件名可能无效的bug

--- ExceedMVC/UI/OtherViews/UIImageView+Cache.m
+++ ExceedMVC/UI/OtherViews/UIImageView+Cache.m
@@ -7,22 +7,24 @@
 //
 
 #import "UIImageView+Cache.h"
+#import <CommonCrypto/CommonDigest.h>
 #import "HTTPConnection.h"
 
 
 // 将url转换为文件名
 NSString *transferFileNameFromURL(NSString *url)
 {
-    //拼接图片文件名
-    NSCharacterSet *setChars = [NSCharacterSet characterSetWithCharactersInString:@":/."];
-    NSArray *components = [url componentsSeparatedByCharactersInSet:setChars];
-    NSMutableString *mstrFileName = [NSMutableString string];
-    for (NSString *component in components) {
-        [mstrFileName appendString:component];
-    }
-    //拼接后缀
-    [mstrFileName appendFormat:@".%@", [[NSURL URLWithString:url] pathExtension]];
-    return mstrFileName;
+    const char *cStr = [url UTF8String];
+    unsigned char result[16];
+    CC_MD5(cStr, (CC_LONG)strlen(cStr), result);
+    NSString *fileName = [NSString stringWithFormat:
+                          @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+                          result[0], result[1], result[2], result[3],
+                          result[4], result[5], result[6], result[7],
+                          result[8], result[9], result[10], result[11],
+                          result[12], result[13], result[14], result[15]];
+    NSString *pathExtension = [[NSURL URLWithString:url] pathExtension];
+    return pathExtension.length>0?[fileName stringByAppendingPathExtension:pathExtension]:fileName;
 }
 
 #pragma mark - UIImageViewManager
