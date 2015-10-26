e5fdd7b18f29ac7c961c06516fcfba79a3c8e027
jianhong <jianhong.yang@chukong-inc.com>
2015-10-26 13:02:12 +0800

    iOS9以后改用NSURLSession
    iOS9以后改用NSURLSession作网络请求，iOS9以前依然使用NSURLConnection

--- ExceedMVC/Core/Net/NetBase/HTTPConnection.m
+++ ExceedMVC/Core/Net/NetBase/HTTPConnection.m
@@ -7,6 +7,7 @@
 //
 
 #import "HTTPConnection.h"
+#import <UIKit/UIKit.h>
 
 
 #define MaxNumber_URLConnection         10
@@ -15,7 +16,7 @@
 #define TaskStatus_Wait      @"Wait"
 
 
-@interface HTTPConnection () {
+@interface HTTPConnection () <NSURLSessionDataDelegate, NSURLSessionDownloadDelegate> {
     NSMutableArray *_marrayTaskDic;
 }
 @property (nonatomic, assign) int numberOfURLConnection;
@@ -71,8 +72,16 @@
     for (NSDictionary *dicTask in _marrayTaskDic) {
         //
         NSURLConnection *connect = dicTask[@"connect"];
-        if ([[[connect originalRequest].URL description] isEqualToString:url]) {
-            return YES;
+        if (connect) {
+            if ([[connect.originalRequest.URL description] isEqualToString:url]) {
+                return YES;
+            }
+        }
+        else {
+            NSURLSessionDataTask *dataTask = dicTask[@"connect"];
+            if ([[dataTask.originalRequest.URL description] isEqualToString:url]) {
+                return YES;
+            }
         }
     }
     return NO;
@@ -131,12 +140,18 @@
     // 状态
     [mdicTask setObject:TaskStatus_Wait forKey:@"status"];
     // 创建HTTP网络连接
-    NSURLConnection *urlConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
-    [mdicTask setObject:urlConnection forKey:@"connect"];
+    if ([[UIDevice currentDevice].systemVersion floatValue] < 9.0) {
+        NSURLConnection *urlConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
+        [mdicTask setObject:urlConnection forKey:@"connect"];
 #if __has_feature(objc_arc)
 #else
-    [urlConnection release];
+        [urlConnection release];
 #endif
+    }
+    else {
+        NSURLSessionDataTask *dataTask = [[NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue]] dataTaskWithRequest:request];
+        [mdicTask setValue:dataTask forKey:@"SessionTask"];
+    }
     // 将下载任务保存到数组
     [_marrayTaskDic addObject:mdicTask];
     
@@ -157,14 +172,21 @@
         if ([dicParam isEqualToDictionary:dicTask[@"param"]]) {
             // 取消网络请求
             NSURLConnection *connect = dicTask[@"connect"];
-            // 未启动的须先启动再取消，不然有内存泄露
-            if ([TaskStatus_Wait isEqualToString:dicTask[@"status"]]) {
-                [connect start];
+            if (connect) {
+                // 未启动的须先启动再取消，不然有内存泄露
+                if ([TaskStatus_Wait isEqualToString:dicTask[@"status"]]) {
+                    [connect start];
+                }
+                else {
+                    self.numberOfURLConnection -= 1;
+                }
+                [connect cancel];
             }
             else {
+                NSURLSessionDataTask *dataTask = dicTask[@"SessionTask"];
+                [dataTask suspend];
                 self.numberOfURLConnection -= 1;
             }
-            [connect cancel];
             // 从任务队列中删除
             [_marrayTaskDic removeObjectAtIndex:i];
             return YES;
@@ -179,22 +201,28 @@
     // 遍历所有任务
     for (NSDictionary *dicTask in _marrayTaskDic) {
         NSURLConnection *connect = dicTask[@"connect"];
-        // 未启动的须先启动再取消，不然有内存泄露
-        if ([TaskStatus_Wait isEqualToString:dicTask[@"status"]]) {
-            [connect start];
+        if (connect) {
+            // 未启动的须先启动再取消，不然有内存泄露
+            if ([TaskStatus_Wait isEqualToString:dicTask[@"status"]]) {
+                [connect start];
+            }
+            else {
+                self.numberOfURLConnection -= 1;
+            }
+            [connect cancel];
         }
         else {
+            NSURLSessionDataTask *dataTask = dicTask[@"SessionTask"];
+            [dataTask suspend];
             self.numberOfURLConnection -= 1;
         }
-        [connect cancel];
     }
     // 从任务队列中删除
     [_marrayTaskDic removeAllObjects];
 }
 
 
-#pragma mark -
-#pragma mark NSURLConnectionDataDelegate
+#pragma mark - NSURLConnectionDataDelegate
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
 {
@@ -314,7 +342,255 @@
 }
 
 
-#pragma mark - ()
+#pragma mark - NSURLSessionDelegate
+
+- (void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(nullable NSError *)error
+{
+    
+}
+
+- (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * __nullable credential))completionHandler
+{
+    if (completionHandler) {
+        completionHandler(NSURLSessionAuthChallengePerformDefaultHandling,
+                          [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]);
+    }
+}
+
+- (void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)session
+{
+    
+}
+
+
+#pragma mark - NSURLSessionTaskDelegate
+
+/* An HTTP request is attempting to perform a redirection to a different
+ * URL. You must invoke the completion routine to allow the
+ * redirection, allow the redirection with a modified request, or
+ * pass nil to the completionHandler to cause the body of the redirection
+ * response to be delivered as the payload of this request. The default
+ * is to follow redirections.
+ *
+ * For tasks in background sessions, redirections will always be followed and this method will not be called.
+ */
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task
+willPerformHTTPRedirection:(NSHTTPURLResponse *)response
+        newRequest:(NSURLRequest *)request
+ completionHandler:(void (^)(NSURLRequest * __nullable))completionHandler
+{
+    
+}
+
+/* The task has received a request specific authentication challenge.
+ * If this delegate is not implemented, the session specific authentication challenge
+ * will *NOT* be called and the behavior will be the same as using the default handling
+ * disposition.
+ */
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task
+didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * __nullable credential))completionHandler
+{
+    
+}
+
+/* Sent if a task requires a new, unopened body stream.  This may be
+ * necessary when authentication has failed for any request that
+ * involves a body stream.
+ */
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task
+ needNewBodyStream:(void (^)(NSInputStream * __nullable bodyStream))completionHandler
+{
+    
+}
+
+/* Sent periodically to notify the delegate of upload progress.  This
+ * information is also available as properties of the task.
+ */
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task
+   didSendBodyData:(int64_t)bytesSent
+    totalBytesSent:(int64_t)totalBytesSent
+totalBytesExpectedToSend:(int64_t)totalBytesExpectedToSend
+{
+    
+}
+
+/* Sent as the last message related to a specific task.  Error may be
+ * nil, which implies that no error occurred and this task is complete.
+ */
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task
+didCompleteWithError:(NSError *)error
+{
+    HTTPLog(@"网络请求错误:%@", error);
+    // 找到当前失败的任务
+    int indexTask = 0;
+    NSDictionary *dicTask = nil;
+    for (int i = 0; i < _marrayTaskDic.count; i++) {
+        NSDictionary *dic = _marrayTaskDic[i];
+        // 找到网络连接相应的数据字典
+        if (dic[@"SessionTask"] == task) {
+            indexTask = i;
+            dicTask = dic;
+            break;
+        }
+    }
+    // 删除失败的任务
+    if (dicTask) {
+        // 删除
+        NSDictionary *dicTempTask = [NSDictionary dictionaryWithDictionary:dicTask];
+        self.numberOfURLConnection -= 1;
+        [_marrayTaskDic removeObjectAtIndex:indexTask];
+        // 启动新任务
+        [self startURLConnection];
+        // 通知上层任务失败
+        if ([self.delegate respondsToSelector:@selector(httpConnect:error:with:)]) {
+            [self.delegate httpConnect:self error:error with:dicTempTask[@"param"]];
+        }
+    }
+}
+
+
+#pragma mark - NSURLSessionDataDelegate
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
+didReceiveResponse:(NSURLResponse *)response
+ completionHandler:(void (^)(NSURLSessionResponseDisposition disposition))completionHandler
+{
+    HTTPLog(@"网络请求收到响应");
+    // 找到相应的任务
+    NSMutableDictionary *mdicTask = nil;
+    for (int i = 0; i < _marrayTaskDic.count; i++) {
+        NSMutableDictionary *mdic = _marrayTaskDic[i];
+        // 找到网络连接相应的数据字典
+        if (mdic[@"SessionTask"] == dataTask) {
+            mdicTask = mdic;
+            break;
+        }
+    }
+    //
+    if ([response isMemberOfClass:NSHTTPURLResponse.class]) {
+        NSHTTPURLResponse *responseHTTP = (NSHTTPURLResponse *)response;
+        NSUInteger statusCode = responseHTTP.statusCode;
+        NSDictionary *dicAllHeaderFields = responseHTTP.allHeaderFields;
+        [mdicTask setValue:dicAllHeaderFields[@"Content-Length"] forKey:@"Content-Length"];
+        NSDictionary *dicParam = mdicTask[@"param"];
+        // 收到服务器返回的HTTP信息头
+        if ([self.delegate respondsToSelector:@selector(httpConnect:receiveResponseWithStatusCode:andAllHeaderFields:with:)]) {
+            [self.delegate httpConnect:self receiveResponseWithStatusCode:statusCode
+                    andAllHeaderFields:dicAllHeaderFields with:dicParam];
+        }
+    }
+    completionHandler(NSURLSessionResponseAllow);
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
+    didReceiveData:(NSData *)data
+{
+    HTTPLog(@"网络请求收到数据");
+    // 找到相应的任务
+    NSDictionary *dicTask = nil;
+    for (int i = 0; i < _marrayTaskDic.count; i++) {
+        NSDictionary *dic = _marrayTaskDic[i];
+        // 找到网络连接相应的数据字典
+        if (dic[@"SessionTask"] == dataTask) {
+            dicTask = dic;
+            break;
+        }
+    }
+    //
+    if (dicTask) {
+        // 向缓存中添加数据
+        NSMutableData *mdataCache = dicTask[@"cache"];
+        [mdataCache appendData:data];
+        NSDictionary *dicParam = dicTask[@"param"];
+        HTTPLog(@"该数据的参数：%@", dicParam);
+        // 所有数据均已接收完成
+        if ([dicTask[@"Content-Length"] longLongValue] > 0 &&
+            mdataCache.length == [dicTask[@"Content-Length"] longLongValue]) {
+            // 删除
+            NSDictionary *dicTempTask = [NSDictionary dictionaryWithDictionary:dicTask];
+            self.numberOfURLConnection -= 1;
+            [_marrayTaskDic removeObject:dicTask];
+            // 启动新任务
+            [self startURLConnection];
+            // 通知上层完成任务
+            if ([self.delegate respondsToSelector:@selector(httpConnect:finish:with:)]) {
+                NSData *dataCache = dicTempTask[@"cache"];
+                NSDictionary *dicParam = dicTempTask[@"param"];
+                [self.delegate httpConnect:self finish:dataCache with:dicParam];
+            }
+        }
+        // 收到部分数据
+        else {
+            if ([self.delegate respondsToSelector:@selector(httpConnect:receivePartData:with:)]) {
+                [self.delegate httpConnect:self receivePartData:data with:dicParam];
+            }
+        }
+    }
+    HTTPLog(@"网络请求收到数据并处理完成");
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
+ willCacheResponse:(NSCachedURLResponse *)proposedResponse
+ completionHandler:(void (^)(NSCachedURLResponse * __nullable cachedResponse))completionHandler
+{
+    HTTPLog(@"网络请求完成");
+    // 找到当前完成的任务
+    int indexTask = 0;
+    NSDictionary *dicTask = nil;
+    for (int i = 0; i < _marrayTaskDic.count; i++) {
+        NSDictionary *dic = _marrayTaskDic[i];
+        // 找到网络连接相应的数据字典
+        if (dic[@"SessionTask"] == dataTask) {
+            indexTask = i;
+            dicTask = dic;
+            break;
+        }
+    }
+    // 删除已经完成的任务
+    if (dicTask) {
+        // 删除
+        NSDictionary *dicTempTask = [NSDictionary dictionaryWithDictionary:dicTask];
+        self.numberOfURLConnection -= 1;
+        [_marrayTaskDic removeObjectAtIndex:indexTask];
+        // 启动新任务
+        [self startURLConnection];
+        // 通知上层完成任务
+        if ([self.delegate respondsToSelector:@selector(httpConnect:finish:with:)]) {
+            NSData *dataCache = dicTempTask[@"cache"];
+            NSDictionary *dicParam = dicTempTask[@"param"];
+            [self.delegate httpConnect:self finish:dataCache with:dicParam];
+        }
+    }
+}
+
+
+#pragma mark - NSURLSessionDownloadDelegate
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+didFinishDownloadingToURL:(NSURL *)location
+{
+    
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+      didWriteData:(int64_t)bytesWritten
+ totalBytesWritten:(int64_t)totalBytesWritten
+totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
+    
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+ didResumeAtOffset:(int64_t)fileOffset
+expectedTotalBytes:(int64_t)expectedTotalBytes
+{
+    
+}
+
+
+#pragma mark - Private
 
 - (void)startURLConnection
 {
@@ -327,7 +603,13 @@
                     [mdicTask setObject:TaskStatus_Run forKey:@"status"];
                     // 启动
                     NSURLConnection *urlConnection = mdicTask[@"connect"];
-                    [urlConnection start];
+                    if (urlConnection) {
+                        [urlConnection start];
+                    }
+                    else {
+                        NSURLSessionDataTask *dataTask = mdicTask[@"SessionTask"];
+                        [dataTask resume];
+                    }
                     break;
                 }
             }
